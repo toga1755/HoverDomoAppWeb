45babb2e9066a53921d98e4735652b4bcf202523
krrn0722 <mur0722@outlook.jp>
2023-12-10 00:26:15 +0900

    フロントエンドビルド（JS,CSS）を実行する

--- src/LaravelReactProject/resources/views/welcome.blade.php
+++ src/LaravelReactProject/resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -130,6 +131,7 @@
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
+                <div id="example"></div>
             </div>
         </div>
     </body>
