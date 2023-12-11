9a51bdd6139800cc86ffb038968e47e375277ede
krrn0722 <mur0722@outlook.jp>
2023-12-11 18:36:11 +0900

    ヘッダーのappbarの配置の完了

--- src/LaravelReactProject/resources/js/components/header/header.jsx
+++ src/LaravelReactProject/resources/js/components/header/header.jsx
@@ -5,30 +5,54 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-// import MenuIcon from "@mui/icons-material/Menu";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+
+function Contents() {
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                display: "flex",
+                m: 1,
+                p: 1,
+                color: "white",
+                border: "none",
+                borderColor: "grey.800",
+                borderRadius: 2,
+                fontSize: "0.875rem",
+                fontWeight: "700",
+                width: "inherit",
+            }}
+        >
+            <IconButton sx={{}}>
+                <ArrowBackIosIcon sx={{ color: "white" }} />
+            </IconButton>
+            <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                    flexGrow: 1,
+                    color: "white",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                }}
+            >
+                ホーバーデモアプリ
+            </Typography>
+        </Box>
+    );
+}
 
 export default function ButtonAppBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    ></IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
-                    {/* <Button color="inherit">Login</Button> */}
+                <Toolbar sx={{ bgcolor: "orange" }}>
+                    <Contents />
                 </Toolbar>
             </AppBar>
         </Box>
     );
 }
+
