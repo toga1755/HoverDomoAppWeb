5491bfc8d586f80db1fabac54b746a64ba923d64
krrn0722 <mur0722@outlook.jp>
2023-12-19 16:07:27 +0900

    お知らせ詳細画面の完成

--- src/LaravelReactProject/resources/js/components/notice/notice_detail.jsx
+++ src/LaravelReactProject/resources/js/components/notice/notice_detail.jsx
@@ -4,6 +4,7 @@ import { Box, Typography } from "@mui/material";
 import { noticeInfo } from "./notices_constant";
 import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { defaultColor } from "../../style/color_constant";
 
 export default function NoticeDetailPage() {
     return (
@@ -12,17 +13,18 @@ export default function NoticeDetailPage() {
                 sx={{
                     display: "flex",
                     flexDirection: "column",
-                    height: "100%",
+                    height: "100vh",
+                    bgcolor: defaultColor,
                 }}
             >
                 <Header />
                 {/* ヘッダーが89.4% */}
                 <Box
                     sx={{
-                        height: "89.4%",
-                        bgcolor: "#fdf2d0",
+                        // height: "89.4%",
                         paddingTop: "10px",
                         paddingBottom: "10px",
+                        bgcolor: defaultColor,
                     }}
                 >
                     <NoticeDetail></NoticeDetail>
@@ -35,13 +37,57 @@ export default function NoticeDetailPage() {
 function NoticeDetail() {
     const params = useParams();
     return (
-        <Box
-            sx={{
-                height: "100%",
-                color: "blue",
-            }}
-        >
-            {params.id}
+        <Box sx={{}}>
+            {/* お知らせタイトル */}
+            <Box
+                sx={{
+                    paddingTop: "10px",
+                    paddingLeft: "40px",
+                    paddingRight: "40px",
+                }}
+            >
+                <Typography
+                    sx={{
+                        fontSize: "28px",
+                        textAlign: "center",
+                    }}
+                >
+                    {noticeInfo[params.id - 1].title}
+                </Typography>
+            </Box>
+            {/* お知らせ画像 */}
+            <Box
+                sx={{
+                    height: "50vh",
+                    padding: "20px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <img
+                    src={noticeInfo[params.id - 1].image}
+                    alt="お知らせ画像"
+                    style={{
+                        height: "100%",
+                        width: "auto",
+                    }}
+                />
+            </Box>
+            {/* お知らせ内容 */}
+            <Box sx={{ padding: "40px", paddingTop: "10px" }}>
+                <Box
+                    sx={{
+                        borderRadius: "20px",
+                        border: "0.5px solid black",
+                        // bgcolor: "yellow",
+                        height: "100%",
+                        padding: "30px",
+                    }}
+                >
+                    <Typography>{noticeInfo[params.id - 1].content}</Typography>
+                </Box>
+            </Box>
         </Box>
     );
 }
