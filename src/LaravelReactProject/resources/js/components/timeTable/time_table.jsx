1c9db2ed7ca4a1aec3fd0e6d0b98e624562a310c
krrn0722 <mur0722@outlook.jp>
2023-12-21 20:17:14 +0900

    画面のUI配置が完了

--- src/LaravelReactProject/resources/js/components/timeTable/time_table.jsx
+++ src/LaravelReactProject/resources/js/components/timeTable/time_table.jsx
@@ -16,71 +16,49 @@ export default function TimeTable() {
                 <Header />
                 {/* ヘッダーが89.4% */}
                 <Box sx={{ height: "100%" }}>
-                    {/* <OpeTitle></OpeTitle>
-                    <OpeInfoList></OpeInfoList> */}
+                    <TimeTableTitle title={"平日"}></TimeTableTitle>
+                    <TimeTableList></TimeTableList>
+                    <TimeTableTitle title={"土日祝"}></TimeTableTitle>
+                    <TimeTableList></TimeTableList>
                 </Box>
             </Box>
         </>
     );
 }
 
-// function OpeTitle() {
-//     return (
-//         <Box
-//             sx={{
-//                 height: "16%",
-//                 bgcolor: "orange",
-//                 borderTop: "1px solid black",
-//                 borderBottom: "1px solid black",
-//                 padding: "20px",
-//                 display: "flex",
-//                 justifyContent: "left",
-//                 alignItems: "center",
-//             }}
-//         >
-//             <Typography sx={{ fontSize: "24px" }}>運行状況</Typography>
-//         </Box>
-//     );
-// }
+function TimeTableTitle(props) {
+    return (
+        <Box
+            sx={{
+                height: "16%",
+                bgcolor: "orange",
+                borderTop: "1px solid black",
+                borderBottom: "1px solid black",
+                padding: "20px",
+                display: "flex",
+                justifyContent: "left",
+                alignItems: "center",
+            }}
+        >
+            <Typography sx={{ fontSize: "24px" }}>{props.title}</Typography>
+        </Box>
+    );
+}
 
-// function OpeInfoList() {
-//     return <Box></Box>;
-// }
-
-// function OpeInfoContent(props) {
-//     return (
-//         <Box
-//             sx={{
-//                 minHeight: "13vh",
-//                 bgcolor: defaultColor,
-//                 borderBottom: "1px solid black",
-//                 display: "flex",
-//                 justifyContent: "left",
-//                 alignItems: "center",
-//                 padding: "20px",
-//             }}
-//         >
-//             <Typography sx={{ fontSize: "16px" }}>{props.content}</Typography>
-//         </Box>
-//     );
-// }
-
-// function OpeInfoContentEmpty() {
-//     return (
-//         <Box
-//             sx={{
-//                 minHeight: "13vh",
-//                 bgcolor: defaultColor,
-//                 borderBottom: "1px solid black",
-//                 display: "flex",
-//                 justifyContent: "left",
-//                 alignItems: "center",
-//                 padding: "20px",
-//             }}
-//         >
-//             <Typography sx={{ fontSize: "16px" }}>
-//                 運行状況はありません。
-//             </Typography>
-//         </Box>
-//     );
-// }
+function TimeTableList() {
+    return (
+        <Box
+            sx={{
+                minHeight: "13vh",
+                bgcolor: defaultColor,
+                borderBottom: "1px solid black",
+                display: "flex",
+                justifyContent: "left",
+                alignItems: "center",
+                padding: "20px",
+            }}
+        >
+            <Typography sx={{ fontSize: "16px" }}>{"aa"}</Typography>
+        </Box>
+    );
+}
